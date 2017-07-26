4b47c33d210cd6013130f23a9023db7a23f1e9c9
Rodrigo Ribeiro Kiguti <rkiguti@gmail.com>
2017-07-26 13:47:30 -0300

    Commit inicial

---
+++ AzureMediaServices/AzureMediaServices.App/AzureMediaServices.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Microsoft.WindowsAzure.MediaServices.Client;
+using UploadProgressChangedEventArgs = Microsoft.WindowsAzure.MediaServices.Client.UploadProgressChangedEventArgs;
+
+namespace AzureMediaServices.App
+{
+	public class AzureMediaServices
+	{
+		#region Campos
+
+		private readonly Lazy<CloudMediaContext> _cloudMediaContextLazy;
+
+		#endregion
+
+		#region Constantes
+		
+		private const string MEDIA_PROCESSOR_NAME = "Media Encoder Standard";
+		private const string VIDEO_TASK_CONFIGURATION = "Adaptive Streaming";
+		private const string AUDIO_TASK_CONFIGURATION = "AAC Stereo for Streaming"; 
+
+		#endregion
+
+		#region Construtores
+
+		public AzureMediaServices(string mediaServicesAccountName, string mediaServicesAccountKey)
+		{
+			_cloudMediaContextLazy = new Lazy<CloudMediaContext>(() =>
+			{
+				var cachedCredentials = new MediaServicesCredentials(mediaServicesAccountName, mediaServicesAccountKey);
+				return new CloudMediaContext(cachedCredentials);
+			});
+		}
+
+		#endregion
+
+		#region Métodos
+
+		public IAsset UploadFile(string fileName, AssetCreationOptions options,
+			Action<IAssetFile, UploadProgressChangedEventArgs> uploadProgressChangedCallback = null)
+		{
+			var inputAsset = _cloudMediaContextLazy.Value.Assets.CreateFromFile(fileName, options, uploadProgressChangedCallback);
+
+			return inputAsset;
+		}
+
+		public IAsset EncodeVideoToAdaptiveBitrate(IAsset asset, AssetCreationOptions options, string outputAssetName, out IJob job,
+			Action<IJob> executionProgressChangedCallback = null)
+		{
+			job = _cloudMediaContextLazy.Value.Jobs.CreateWithSingleTask(MEDIA_PROCESSOR_NAME, VIDEO_TASK_CONFIGURATION, asset,
+				outputAssetName, options);
+
+			job.Submit();
+
+			job = job.StartExecutionProgressTask(executionProgressChangedCallback, CancellationToken.None).Result;
+
+			var outputAsset = job.OutputMediaAssets[0];
+
+			return outputAsset;
+		}
+
+		public IAsset EncodeVideoToAdaptiveBitrate(IAsset asset, AssetCreationOptions options, string outputAssetName,
+			Action<IJob> executionProgressChangedCallback = null)
+		{
+			return EncodeVideoToAdaptiveBitrate(asset, options, outputAssetName, out var _, executionProgressChangedCallback);
+		}
+
+		public IAsset EncodeAudioToAacStereo(IAsset asset, AssetCreationOptions options, string outputAssetName, out IJob job,
+			Action<IJob> executionProgressChangedCallback = null)
+		{
+			job = _cloudMediaContextLazy.Value.Jobs.CreateWithSingleTask(MEDIA_PROCESSOR_NAME, AUDIO_TASK_CONFIGURATION, asset,
+				outputAssetName, options);
+
+			job.Submit();
+
+			job = job.StartExecutionProgressTask(executionProgressChangedCallback, CancellationToken.None).Result;
+
+			var outputAsset = job.OutputMediaAssets[0];
+
+			return outputAsset;
+		}
+
+		public IAsset EncodeAudioToAacStereo(IAsset asset, AssetCreationOptions options, string outputAssetName,
+			Action<IJob> executionProgressChangedCallback = null)
+		{
+			return EncodeAudioToAacStereo(asset, options, outputAssetName, out var _, executionProgressChangedCallback);
+		}
+
+		private void CreateOnDemandLocator(IAsset asset)
+		{
+			_cloudMediaContextLazy.Value.Locators.Create(
+				LocatorType.OnDemandOrigin,
+				asset,
+				AccessPermissions.Read,
+				TimeSpan.FromDays(30));
+		}
+
+		public void PublishAssetForStreaming(IAsset asset, out Uri smoothStreamingUri, out Uri hlsUri, out Uri mpegDashUri)
+		{
+			CreateOnDemandLocator(asset);
+
+			smoothStreamingUri = asset.GetSmoothStreamingUri();
+			hlsUri = asset.GetHlsUri();
+			mpegDashUri = asset.GetMpegDashUri();
+		}
+
+		public IAsset GetAsset(string assetId)
+		{
+			var assetInstance =
+				from a in _cloudMediaContextLazy.Value.Assets
+				where a.Id == assetId
+				select a;
+
+			var asset = assetInstance.FirstOrDefault();
+
+			return asset;
+		}
+
+		public void DeleteAsset(IAsset asset)
+		{
+			asset.Delete();
+		}
+
+		public IJob GetJob(string jobId)
+		{
+			var jobInstance =
+				from j in _cloudMediaContextLazy.Value.Jobs
+				where j.Id == jobId
+				select j;
+			
+			var job = jobInstance.FirstOrDefault();
+
+			return job;
+		}
+
+		public void DeleteJob(string jobId)
+		{
+			var jobDeleted = false;
+
+			while (!jobDeleted)
+			{
+				var job = GetJob(jobId);
+
+				switch (job.State)
+				{
+					case JobState.Finished:
+					case JobState.Canceled:
+					case JobState.Error:
+						job.Delete();
+						jobDeleted = true;
+						break;
+
+					case JobState.Canceling:
+						Thread.Sleep(5000);
+						break;
+
+					case JobState.Queued:
+					case JobState.Scheduled:
+					case JobState.Processing:
+						job.Cancel();
+						break;
+
+					default:
+						throw new ArgumentException("JobState invalid");
+				}
+
+			}
+		}
+
+		public IEnumerable<IAsset> ListAssets()
+		{
+			return _cloudMediaContextLazy.Value.Assets;
+		}
+
+		public long TotalFileSize(IAsset asset)
+		{
+			long totalFileSize = 0;
+
+			foreach (var assetFile in asset.AssetFiles)
+			{
+				totalFileSize += assetFile.ContentFileSize;
+			}
+
+			return totalFileSize;
+		}
+
+		public long TotalFileSize()
+		{
+			long totalFileSize = 0;
+
+			foreach (var asset in ListAssets())
+			{
+				totalFileSize += TotalFileSize(asset);
+			}
+
+			return totalFileSize;
+		}
+
+		public void DownloadAsset(IAsset asset, string outputDirectory)
+		{
+			foreach (var file in asset.AssetFiles)
+			{
+				file.Download(Path.Combine(outputDirectory, file.Name));
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ AzureMediaServices/AzureMediaServices.App/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.WindowsAzure.MediaServices.Client;
+
+namespace AzureMediaServices.App
+{
+	class Program
+	{
+		// Read values from the App.config file.
+		private static readonly string _mediaServicesAccountName =
+			ConfigurationManager.AppSettings["MediaServicesAccountName"];
+		private static readonly string _mediaServicesAccountKey =
+			ConfigurationManager.AppSettings["MediaServicesAccountKey"];
+
+		// Field for service context.
+		private static AzureMediaServices azureMediaServices = null;
+
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine($"Start: {DateTime.Now}");
+				Console.WriteLine();
+
+				azureMediaServices = new AzureMediaServices(_mediaServicesAccountName, _mediaServicesAccountKey);
+
+				var videoAsset = UploadFile(@"D:\DRIVE_C\Downloads\06. Demo - Basics.mp4");
+				var audioAsset = UploadFile(@"D:\Guide 6 Understand.mp3");
+
+				var videoEncodedAsset = EncodeVideo(videoAsset, "videoAsset", out var videoJob);
+				var audioEncodedAsset = EncodeAudio(audioAsset, "audioAsset", out var audioJob);
+
+				PublishAssetGetURLs(videoEncodedAsset);
+				PublishAssetGetURLs(audioEncodedAsset);
+
+				azureMediaServices.DeleteJob(videoJob.Id);
+				azureMediaServices.DeleteJob(audioJob.Id);
+
+				azureMediaServices.DeleteAsset(videoAsset);
+				azureMediaServices.DeleteAsset(audioAsset);
+
+				PrintTotalFileSize();
+
+				Console.WriteLine($"Finish: {DateTime.Now}");
+			}
+			catch (Exception exception)
+			{
+				// Parse the XML error message in the Media Services response and create a new
+				// exception with its content.
+				exception = MediaServicesExceptionParser.Parse(exception);
+
+				Console.Error.WriteLine(exception.Message);
+			}
+			finally
+			{
+				Console.ReadLine();
+			}
+		}
+
+		static public IAsset UploadFile(string fileName)
+		{
+			IAsset inputAsset = azureMediaServices.UploadFile(fileName, AssetCreationOptions.None, 
+				(af, p) =>
+				{
+					Console.WriteLine("Uploading '{0}' - Progress: {1:0.##}%", af.Name, p.Progress);
+				});
+
+			Console.WriteLine("Asset {0} created.", inputAsset.Id);
+
+			return inputAsset;
+		}
+
+		static public IAsset EncodeVideo(IAsset asset, string outputName, out IJob job)
+		{
+			Console.WriteLine("Submitting video transcoding job...");
+
+			var outputAsset = azureMediaServices.EncodeVideoToAdaptiveBitrate(asset, AssetCreationOptions.None, outputName, out job, 
+				j =>
+				{
+					Console.WriteLine("Job state: {0}", j.State);
+					Console.WriteLine("Job progress: {0:0.##}%", j.GetOverallProgress());
+				});
+
+			Console.WriteLine("Video transcoding job finished.");
+
+			return outputAsset;
+		}
+
+		static public IAsset EncodeAudio(IAsset asset, string outputName, out IJob job)
+		{
+			Console.WriteLine("Submitting audio transcoding job...");
+
+			var outputAsset = azureMediaServices.EncodeAudioToAacStereo(asset, AssetCreationOptions.None, outputName, out job,
+				j =>
+				{
+					Console.WriteLine("Job state: {0}", j.State);
+					Console.WriteLine("Job progress: {0:0.##}%", j.GetOverallProgress());
+				});
+
+			Console.WriteLine("Audio transcoding job finished.");
+
+			return outputAsset;
+		}
+
+		static public void PublishAssetGetURLs(IAsset asset)
+		{
+			azureMediaServices.PublishAssetForStreaming(asset, out var smoothStreamingUri, out var hlsUri, out var mpegDashUri);
+
+			// Display  the streaming URLs.
+			Console.WriteLine("Use the following URLs for adaptive streaming: ");
+			Console.WriteLine(smoothStreamingUri);
+			Console.WriteLine(hlsUri);
+			Console.WriteLine(mpegDashUri);
+			Console.WriteLine();
+		}
+
+		static public void PrintTotalFileSize()
+		{
+			Console.WriteLine($"Total file size: {azureMediaServices.TotalFileSize() / 1000.0 / 1000.0:0.00} MB");
+			Console.WriteLine();
+		}
+	}
+}
